d740df988328e80ed26a802862594e1673cd9982
ppiotrowicz <ppiotrowicz@gmail.com>
2012-11-08 20:56:17 +0100

    First spike with taskbar progress

--- src/Pomidoro/MainWindow.xaml.cs
+++ src/Pomidoro/MainWindow.xaml.cs
@@ -1,27 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pomidoro
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1.0d),
+            };
+
+            _timer.Tick += OnTimerTick;
+
+            _timer.IsEnabled = true;
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            Value += 0.05d;
+
+            if (Value >= 1.0d)
+            {
+                //finish
+                _timer.IsEnabled = false;
+            }
+        }
+
+        private double _value;
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                NotifyPropertyChanged("Value");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
+
 }
